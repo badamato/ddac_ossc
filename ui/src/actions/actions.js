612e0602b9070399f8ffcdf4bb9d529780c3a953
Beth Damato <bethadamato@gmail.com>
2019-07-04 11:33:36 -0400

    adds styling

--- ui/src/actions/actions.js
+++ ui/src/actions/actions.js
@@ -8,11 +8,10 @@ import { streamingRequest } from '../common/requests.js';
 const hostname = window.location.hostname;
 
 export function writeApi() {
-    var data = '{"dc": "AWS", "count": 200000, "cl": "ONE"}';
+    var data = '{"targetCluster": "dc0"}';
 
     return(dispatch, getState) => {
         dispatch(appendValue('events', 'Initiating writes for purchase transactions'))
-        // dispatch(updateValue("snackbarOpen", true))
 
         const url = 'http://'+hostname+':5000/demo/write';
         streamingRequest({
@@ -35,7 +34,6 @@ export function readApi() {
 
     return(dispatch, getState) => {
         dispatch(appendValue('events', 'Initiating reads for purchase transactions'))
-        // dispatch(updateValue("snackbarOpen", true))
 
         const url = 'http://'+hostname+':5000/demo/read';
         streamingRequest({
@@ -59,9 +57,7 @@ export function readApi() {
 //             success: function(res){
 //                 dispatch(updateValue('nodeList', res.data))
 
-//                 //TODO: Get the data centers from res.data and assign them to:
 //                 let rawList = []
-
 //                 rawList = res.data.map((data) => {
 //                     if(data.dc) {
 //                         return data.dc
@@ -140,146 +136,6 @@ export function getNodeInfo() {
     }
 }
 
-// export function dropOneNode() {
-//     return(dispatch, getState) => {
-//         dispatch(appendValue('events', 'Taking down node'))
-//         // dispatch(updateValue("snackbarOpen", true))
-
-//         const url = 'http://'+hostname+':8080/demo/killnode';
-//         const nodeIpAddresses = getState().app.nodeList
-//           .filter((node) => {
-//               return node.dc === 'AWS';
-//           })
-//           .filter((node) => {
-//               return node.mode === 'normal';
-//           }).map(node => {
-//               return node.node_ip
-//           }) 
-//         const randomDroppedNode = nodeIpAddresses[parseInt(Math.random() * nodeIpAddresses.length)]
-
-//         // mark as stopping
-//         const nl = getState().app.nodeList.map((node, i) => {
-//             if (node.node_ip === randomDroppedNode) {
-//                 node.mode = "stopping"
-//                 node.last_seen = -2
-//             } 
-//             return node
-//         })
-//         dispatch(updateValue("nodeList", nl));
- 
-//         console.log([randomDroppedNode]) 
-//         if (randomDroppedNode !== undefined) {
-//             post({
-//                 url: url,
-//                 params: [randomDroppedNode],
-//                 success: function(res){
-
-//                 },
-//                 dispatch: dispatch,
-//                 method: "POST"
-//             })
-//         }
-//     }
-// }
-
-// export function dropOneDataCenter() {
-//     var awsDataCenter = '{"dc": "AWS", "scenario": 3}';
-//     // var gcpDataCenter = '{"dc": "GCP", "scenario": 3}';
-//     // var azureDataCenter = '{"dc": "Azure", "scenario": 3}';
-
-//     return(dispatch, getState) => {
-//         dispatch(appendValue('events', 'Taking down DC'))
-//         // dispatch(updateValue("snackbarOpen", true))
-
-//         const url = 'http://'+hostname+':8080/demo/chaos';
-//         // const gatherDataCenters = []
-//         // gatherDataCenters.push(awsDataCenter, gcpDataCenter, azureDataCenter)
-//         // const randomDataCenter = gatherDataCenters[parseInt(Math.random() * gatherDataCenters.length)]
-//         const data = awsDataCenter
-//         console.log(data)
-//         post({
-//             url: url,
-//             params: data,
-//             success: function(res){
-                
-//             },
-//             dispatch: dispatch,
-//             method: "POST"
-//         })
-
-//         // mark as stopping
-//         const nl = getState().app.nodeList.map((node, i) => {
-//             if (node.dc === "AWS") {
-//                 node.mode = "stopping"
-//                 node.last_seen = -2
-//             } 
-//             return node
-//         })
-//         dispatch(updateValue("nodeList", nl));
- 
-//     }
-// }
-
-// export function resetAllNodes() {
-//     return(dispatch, getState) => {
-//         dispatch(appendValue('events', 'Bringing node(s) back online'))
-//         // dispatch(updateValue("snackbarOpen", true))
-
-//         const url = 'http://'+hostname+':8080/demo/recover';
-//         const nodesDown = [];
-//         const nl = getState().app.nodeList.map((node, i) => {
-//             if (node.mode === null) {
-//                 nodesDown.push(node.node_ip)
-//                 node.mode = "starting"
-//                 node.last_seen = -1
-//             } 
-//             return node
-//         })
-//         dispatch(updateValue("nodeList", nl));
-//         console.log(nodesDown)
-//         post({
-//             url: url,
-//             params: nodesDown,
-//             success: function(res){
-//             },
-//             dispatch: dispatch,
-//             method: "POST"
-//         })
-//     }
-// }
-
-// export function rollingRestart() {
-//     // var awsDataCenter = '{"dc": "AWS", "scenario": 4, "rrdelay": 130}';
-//     var gcpDataCenter = '{"dc": "GCP", "scenario": 4, "rrdelay": 130}';
-//     // var azureDataCenter = '{"dc": "Azure", "scenario": 4, "rrdelay": 130}';
-
-//     return(dispatch, getState) => {
-//         dispatch(appendValue('events', 'Rolling restart'))
-//         // dispatch(updateValue("snackbarOpen", true))
-
-//         const url = 'http://'+hostname+':8080/demo/chaos';
-
-//         // const gatherDataCenters = []
-//         // gatherDataCenters.push(awsDataCenter, gcpDataCenter, azureDataCenter)
-//         // const randomDataCenter = gatherDataCenters[parseInt(Math.random() * gatherDataCenters.length)]
-//         // console.log(gatherDataCenters[0])
-//         post({
-//             url: url,
-//             params: gcpDataCenter,
-//             success: function(res){
-//             },
-//             dispatch: dispatch,
-//             method: "POST"
-//         })
-//     }
-// }
-
-// export function snackbarToggle(snackbarStatus) {
-//     return(dispatch, getState) => {
-//         dispatch(updateValue("snackbarOpen", snackbarStatus))
-//     }
-// }
-
 export function updateValue(key, value){
     return(dispatch, getState) => {
             dispatch(updateData("UPDATE", {"key": key, "value": value}))
@@ -303,5 +159,4 @@ export const updateData = (type, data) => {
     }
 }
 
-// export default {writeApi, readApi, readChunk, getDataCenter, getNodeInfo, dropOneNode, dropOneDataCenter, resetAllNodes, rollingRestart, snackbarToggle, updateValue, appendValue, updateData};
 export default {writeApi, readApi, getNodeInfo, updateValue, appendValue, updateData};
